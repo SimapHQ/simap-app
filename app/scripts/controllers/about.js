6738cc4de487ea3d44f6a0cf277c943ce20b16f8
Ben Draut <drautb@familysearch.org>
2014-08-31 15:32:56 -0600

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name foodStorageTrackerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the foodStorageTrackerApp
+ */
+angular.module('foodStorageTrackerApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name foodStorageTrackerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the foodStorageTrackerApp
+ */
+angular.module('foodStorageTrackerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
