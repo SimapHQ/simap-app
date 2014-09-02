5b5c7ff09fcf5445beff5bc65c3c7b562ad43e72
Ben Draut <drautb@gmail.com>
2014-09-01 22:53:26 -0600

    Started deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eux
+
+# Build the production version.
+grunt build
+
+# Checkout the gh-pages branch
+
+
+# Get rid of everything but the dist directory
+
+
+# Push it to GitHub
+
+
+# Switch back to master
+gcm
+
+ 
