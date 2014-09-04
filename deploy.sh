abcbe584bb665b3789a3ce9f325275f281e7e7b6
Ben Draut <drautb@familysearch.org>
2014-09-04 10:37:39 -0600

    Basic deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
-
 set -eux
 
-# Build the production version.
+PAGES_BRANCH="gh-pages"
+COMMIT_MESSAGE="Food Storage Tracker v$1"
+
+echo "Building production files"
 grunt build
 
-# Checkout the gh-pages branch
+echo "Moving files to /tmp"
+rm -rf /tmp/dist
+mv dist /tmp
 
+echo "Switching to ${PAGES_BRANCH} branch"
+git checkout --orphan gh-pages
 
-# Get rid of everything but the dist directory
+echo "Removing all files from working tree"
+rm -rf *
+git rm -rf .
 
+echo "Adding files from /tmp/dist"
+cp -R /tmp/dist/* .
+rm -rf /tmp/dist
+
+echo "Committing with message ${COMMIT_MESSAGE}"
+git add .
+git commit -m "${COMMIT_MESSAGE}"
 
 # Push it to GitHub
-
+# git push origin gh-pages
 
 # Switch back to master
 gcm
