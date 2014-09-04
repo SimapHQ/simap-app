2a90f69bdcfa85b18c20b19c40c62775302a02fe
Ben Draut <drautb@familysearch.org>
2014-09-04 16:34:08 -0600

    Started adding units to item screen.

--- app/scripts/controllers/item.js
+++ app/scripts/controllers/item.js
@@ -1,5 +1,25 @@
 'use strict';
 
 angular.module('foodStorageTrackerApp').controller('ItemCtrl', function ($scope) {
-  $scope.blah = 'blah';
+  $scope.item = {
+    units: [
+      { name: "cup", masterUnits: 1 },
+      { name: "lb", masterUnits: 25 }
+    ]
+  };
+
+  $scope.addUnit = function() {
+    if (!$scope.newUnitName || $scope.newUnitName === '' ||
+        !$scope.newUnitMasterUnits || $scope.newUnitMasterUnits === '') {
+      return;
+    }
+
+    $scope.item.units.push({
+      name: $scope.newUnitName,
+      masterUnits: $scope.newUnitMasterUnits
+    });
+
+    $scope.newUnitName = '';
+    $scope.newUnitMasterUnits = '';
+  };
 });
