42f4392a7e966db5906aeb23de94b3cfd5d67d86
Ben Draut <drautb@familysearch.org>
2014-09-08 16:47:18 -0600

    DRYed planning page up.

--- app/scripts/controllers/planning.js
+++ app/scripts/controllers/planning.js
@@ -1,5 +1,7 @@
 'use strict';
 
 angular.module('simapApp').controller('PlanningCtrl', function ($scope) {
-  $scope.blah = 'blah';
+  $scope.helpBlock = 'Once you put in the number of adults and children in your family, Simap can make sure your inventory is sufficient.';
+
+  $scope.preparedUntil = 'Meeting this goal means you\'ll be prepared until September 4, 2087';
 });
