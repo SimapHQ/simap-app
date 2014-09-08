3c318c64e0d6df0d76a4f865f2861870e3884fee
Ben Draut <drautb@familysearch.org>
2014-09-08 13:41:44 -0600

    Further DRYing

--- app/scripts/controllers/categories.js
+++ app/scripts/controllers/categories.js
@@ -2,36 +2,20 @@
 
 var app = angular.module('simapApp');
 
-app.controller('CategoriesCtrl', function ($scope) {
+app.controller('CategoriesCtrl', function ($scope, $location) {
+  $scope.helpBlock = 'Categories let you group your items. Here you can select a category to edit, delete a category, and add a new category.';
 
   $scope.categories = ['Vegetables', 'Meats and Proteins', 'Household Supplies'];
-  $scope.newCategory = '';
 
-  /**
-   * This shouldn't allow you to add a category that already exists.
-   */
-  $scope.addCategory = function() {
-    if ($scope.newCategory === '' || $scope.categories.indexOf($scope.newCategory) !== -1) {
-      return;
-    }
-    $scope.categories.push($scope.newCategory);
-    $scope.newCategory = '';
+  $scope.addNewCategory = function() {
+    $location.path('/category/new');
   };
 
-  /**
-   * This shouldn't allow you to rename a category to duplicate another.
-   * This shouldn't allow you to rename a category to ''.
-   */
   $scope.editCategory = function() {
+    $location.path('/category/edit/categoryid');
+  };
+
+  $scope.removeCategory = function() {
     
   };
-
-  /**
-   * This shouldn't let you remove a category that has items in it.
-   */
-  $scope.removeCategory = function(categoryToRemove) {
-    var idxOfCategoryToRemove = $scope.categories.indexOf(categoryToRemove);
-    $scope.categories.splice(idxOfCategoryToRemove, 1);
-  };
-
 });
