b3985f4d9ed8e9801e9e3a5946bafbd83bd71417
Ben Draut <drautb@gmail.com>
2014-10-29 12:44:57 -0600

    Goal working!

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -13,4 +13,7 @@ app.constant('GOAL_NODE', 'goal/');
 app.constant('DEFAULT_FAMILY_SIZE_ADULTS', 1);
 app.constant('DEFAULT_FAMILY_SIZE_CHILDREN', 0);
 
+// From https://www.google.com/webhp?ion=1&espv=2&ie=UTF-8#q=number%20of%20days%20in%20months
+app.constant('DAYS_IN_MONTH', 30.4368);
+
 app.constant('DEFAULT_GOAL_DAYS', 90);
--- app/scripts/planning/goalCtrl.js
+++ app/scripts/planning/goalCtrl.js
@@ -3,21 +3,39 @@
 angular.module('simapApp').controller('GoalCtrl', [
   '$firebase',
   '$scope',
-  'GOAL_NODE',
+  'DAYS_IN_MONTH',
   'FirebaseService',
+  'GOAL_NODE',
   'GoalService',
   'SessionService',
   function (
     $firebase,
     $scope,
-    GOAL_NODE,
+    DAYS_IN_MONTH,
     FirebaseService,
+    GOAL_NODE,
     GoalService,
     SessionService
     ) {
 
-    var ref = FirebaseService.getRef();
-    var goal_id = SessionService.currentSession().goal_id;
+    var ref = FirebaseService.getRef(),
+        goal_id = SessionService.currentSession().goal_id;
 
-    $firebase(ref.child(GOAL_NODE + goal_id + '/days')).$asObject().$bindTo($scope, 'days');
+    $firebase(ref.child(GOAL_NODE + goal_id + '/days')).$asObject().$bindTo($scope, 'days').then(function() {
+      $scope.months = Math.round($scope.days.$value / DAYS_IN_MONTH);
+      $scope.updatePreparedUntilDate();
+    });
+
+    $scope.monthsChanged = function() {
+      if ($scope.months === undefined || $scope.months === null) {
+        return;
+      }
+
+      $scope.days.$value = Math.round($scope.months * DAYS_IN_MONTH);
+      $scope.updatePreparedUntilDate();
+    };
+
+    $scope.updatePreparedUntilDate = function() {
+      $scope.preparedUntilDate = Date.today().add($scope.months).months().toString('MMMM dd, yyyy');
+    };
 }]);
