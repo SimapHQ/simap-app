e74effde821f977fc43b8c235df7c5981dbc517a
Ben Draut <drautb@gmail.com>
2014-10-30 06:03:35 -0600

    Use simap-test target for development.

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -5,7 +5,7 @@ var app = angular.module('simapApp');
 app.constant('HOME', '/home');
 app.constant('LOGIN', '/login');
 
-app.constant('FIREBASE_URL', 'https://simap.firebaseio.com/');
+app.constant('FIREBASE_URL', 'https://simap-test.firebaseio.com/');
 app.constant('USER_NODE', 'user/');
 app.constant('FAMILY_NODE', 'family/');
 app.constant('GOAL_NODE', 'goal/');
