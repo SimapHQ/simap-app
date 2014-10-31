a7b3aa5e803bc456f80dcbf5539f27db59b0b84b
Ben Draut <drautb@gmail.com>
2014-10-31 16:43:45 -0600

    Unit test Category controller.

--- test/unit/spec/categories/categoryCtrlTest.js
+++ test/unit/spec/categories/categoryCtrlTest.js
@@ -2,29 +2,56 @@
 
 describe('Controller: CategoryCtrl', function () {
 
-  var CategoryCtrl, 
+  var syncedCategory,
+      $firebaseObj,
+      mockFirebaseRef;
+
+  var CategoryCtrl,
+      $firebase,
+      $routeParams,
       $scope,
-      $location,
-      randomColor;
+      FirebaseService;
 
   beforeEach(function() {
-    module('mock.firebase');
     module('simapApp');
   });
 
   beforeEach(inject(function ($controller, $rootScope) {
+    syncedCategory = jasmine.createSpyObj('syncedCategory', ['$bindTo']);
+
+    $firebaseObj = jasmine.createSpyObj('$firebaseObj', ['$asObject']);
+    $firebaseObj.$asObject.and.returnValue(syncedCategory);
+
+    $firebase = jasmine.createSpy('$firebase').and.returnValue($firebaseObj);
+
+    mockFirebaseRef = jasmine.createSpyObj('mockFirebaseRef', ['child']);
+    mockFirebaseRef.child.and.callFake(function(node) { return node; });
+
+    FirebaseService = jasmine.createSpyObj('FirebaseService', ['getRef']);
+    FirebaseService.getRef.and.returnValue(mockFirebaseRef);
+
+    $routeParams = { categoryId: 'the-cat-id' };
+
     $scope = $rootScope.$new();
-    randomColor = jasmine.createSpy('randomColor');
 
     CategoryCtrl = $controller('CategoryCtrl', {
+      $firebase: $firebase,
+      $routeParams: $routeParams,
       $scope: $scope,
-      $location: $location,
-      randomColor: randomColor
+      FirebaseService: FirebaseService
     });
   }));
 
-  xit('should let the user save changes to a category', function() {
+  it('should get the firebase reference from FirebaseService', function() {
+    expect(FirebaseService.getRef).toHaveBeenCalled();
+  })
 
+  it('should call $firebase using the right category node', function() {
+    expect($firebase).toHaveBeenCalledWith('category/the-cat-id');
   });
-  
+
+  it('should call bind the node to "category" on the scope', function() {
+    expect(syncedCategory.$bindTo).toHaveBeenCalledWith($scope, 'category');
+  });
+
 });
