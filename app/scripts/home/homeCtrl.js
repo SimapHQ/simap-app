f3f3f6d44481b55818bd6cf58a161036eff6d885
Ben Draut <drautb@gmail.com>
2014-11-07 06:47:39 -0700

    Load all data into home page controller.
    It doesn't show anything yet, but everything gets loaded.

--- app/scripts/home/homeCtrl.js
+++ app/scripts/home/homeCtrl.js
@@ -2,46 +2,62 @@
 
 angular.module('simapApp').controller('HomeCtrl', [
   '$scope',
+  'CONVERSION_NODE',
   'FirebaseService',
   'ITEM_NODE',
   'ListService',
+  'PLAN_NODE',
   'SessionService',
-  'UNIT_NODE',
+  'UnitService',
   function (
     $scope,
+    CONVERSION_NODE,
     FirebaseService,
     ITEM_NODE,
     ListService,
+    PLAN_NODE,
     SessionService,
-    UNIT_NODE
+    UnitService
   ) {
 
   var refreshHomeData = function() {
     $scope.categories = ListService.getList('categories');
+
     $scope.items = {};
     $scope.units = {};
     $scope.plans = {};
     $scope.conversions = {};
 
-    // Object.keys(SessionService.currentSession().items).forEach(function(itemId) {
-    //   $scope.items[itemId] = loadItem(itemId)
-    // });
+    Object.keys(SessionService.currentSession().items).forEach(function(itemId) {
+      loadItem(itemId).then(function(loadedItem) {
+        $scope.items[itemId] = loadedItem;
+
+        FirebaseService.getRef().child(PLAN_NODE + loadedItem.plan_id).once('value', function(dataSnapshot) {
+          $scope.plans[itemId] = dataSnapshot.val();
+        });
+
+        Object.keys(loadedItem.units).forEach(function(unitId) {
+          UnitService.getName(unitId).then(function(unitName) {
+            $scope.units[unitId] = {name: unitName};
+          });
+
+          FirebaseService.getRef().child(CONVERSION_NODE + unitId).once('value', function(dataSnapshot) {
+            $scope.conversions[unitId] = dataSnapshot.val();
+          });
+        });
+      });
+    });
   };
 
-  // var loadItem = function(itemId) {
-  //   var itemObj = FirebaseService.getObject(ITEM_NODE + itemId);
+  // Items are the only things that will be modified from this screen,
+  // so they're the only ones that we keep a connection for.
+  var loadItem = function(itemId) {
+    var itemObj = FirebaseService.getObject(ITEM_NODE + itemId);
 
-  //   return itemObj.$loaded().then(function() {
-  //     var unitPromises = [];
-  //     Object.keys(itemObj.units).forEach(function(unitId) {
-  //       unitPromises.push(FirebaseService.getObject(UNIT_NODE + unitId).$loaded().then(function() {
-  //         $scope.units[itemId].units
-  //       }));
-  //     });
-
-  //     return itemObj;
-  //   });
-  // };
+    return itemObj.$loaded().then(function() {
+      return itemObj;
+    });
+  };
 
   refreshHomeData();
 
