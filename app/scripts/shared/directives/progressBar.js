c1409034774ef437d88114e0a0a897d3c2c2d2f4
Ben Draut <drautb@gmail.com>
2014-11-12 06:06:23 -0700

    Refactor progress bar implementation.
    The link function is only run the first time the directive is seen. This
    meant that, even if the progress bar items were recalculated, the bar
    wouldn't change until the controller was reloaded. By moving the logic
    into the template, Angular picks up on the changes appropriately.

--- app/scripts/shared/directives/progressBar.js
+++ app/scripts/shared/directives/progressBar.js
@@ -4,22 +4,7 @@ var app = angular.module('simapApp');
 
 app.directive('simapProgressBar', function() {
   var linkFn = function(scope, element) {
-    var barWidth = element[0].offsetWidth;
-    var items = scope.items;
-
-    Object.keys(items).forEach(function(item) {
-      item = items[item];
-      if (item.width === 0.0) {
-        return;
-      }
-
-      var segment =  angular.element('<div class=\"segment\"></div>');
-      segment.css({
-        backgroundColor: item.color,
-        width: item.width * barWidth
-      });
-      element.append(segment);
-    });
+    scope.barWidth = element[0].offsetWidth;
   };
 
   return {
