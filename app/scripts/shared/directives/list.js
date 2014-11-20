2cf3c9043b97a76a3ecfef51d67772bdf2dcbc2e
Ben Draut <drautb@gmail.com>
2014-11-19 17:12:38 -0700

    Made Items and Categories lists alphabetical.
    Starts #39.

--- app/scripts/shared/directives/list.js
+++ app/scripts/shared/directives/list.js
@@ -1,8 +1,15 @@
 'use strict';
 
 var app = angular.module('simapApp');
- 
+
 app.directive('simapList', function() {
+  var linkFn = function(scope) {
+    scope.dataArray = [];
+    Object.keys(scope.data).forEach(function(id) {
+        scope.dataArray.push(scope.data[id]);
+    });
+  };
+
   return {
     restrict: 'E',
     scope: {
@@ -11,6 +18,7 @@ app.directive('simapList', function() {
         edit: '&onEdit',
         remove: '&onRemove'
     },
-    templateUrl: 'views/templates/list.html'
+    templateUrl: 'views/templates/list.html',
+    link: linkFn
   };
 });
